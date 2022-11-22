82ca9a3a5857468d170942670ce351e4ca53b822
Maximilian Schwarz <maximilian@unique-web.de>
2022-11-22 10:17:10 +0100

    add documentation

---
+++ docs/app.config.ts
@@ -0,0 +1,10 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Nuxt Nhost',
+    description: 'Docs for Nuxt 3 modules Nhost',
+    layout: 'default',
+    socials: {
+      github: 'maximilian-schwarz/nhost-nuxt3-plugin'
+    }
+  }
+})
